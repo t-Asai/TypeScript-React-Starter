9c767308d9970b1d48f52284125a3e2355fec905
t-Asai <takumi.a42@gmail.com>
2018-08-04 17:36:35 +0900

    drop down menuにlink機能を追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,33 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import {
   Dropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
 } from "reactstrap";
+import routes from "~/routes";
 
-const Header = ({ isOpen, changeMenuIsOpen }: any) => (
-  <Dropdown isOpen={isOpen} toggle={changeMenuIsOpen}>
-    <DropdownToggle>Dropdown</DropdownToggle>
-    <DropdownMenu>
-      <DropdownItem>Header</DropdownItem>
-      <DropdownItem>Action</DropdownItem>
-      <DropdownItem>Another Action</DropdownItem>
-      <DropdownItem />
-      <DropdownItem>Another Action</DropdownItem>
-    </DropdownMenu>
-  </Dropdown>
-);
+const Header = ({ isOpen, changeMenuIsOpen }: any): any => {
+  const item = routes.map(route => {
+    if (route.name !== undefined) {
+      return (
+        <li key={route.path}>
+          <DropdownItem>
+            <Link to={route.path}>{route.name}</Link>
+          </DropdownItem>
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+  return (
+    <Dropdown isOpen={isOpen} toggle={changeMenuIsOpen}>
+      <DropdownToggle>Links</DropdownToggle>
+      <DropdownMenu>{item}</DropdownMenu>
+    </Dropdown>
+  );
+};
 
 export default Header;
