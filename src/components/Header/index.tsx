7c618da008642ae2839616691e66d11d8ab5cddb
t-Asai <takumi.a42@gmail.com>
2018-08-05 00:20:03 +0900

    だいたいこんな感じ

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,60 +8,75 @@ import {
 } from "reactstrap";
 import _routes from "~/routes";
 
-const items = (
-  routes: any,
-  isOpen = false,
-  changeMenuIsOpen: any,
-  name: string
-) => (
-  <Dropdown
-    isOpen={isOpen}
-    toggle={changeMenuIsOpen}
-    // direction="right"
-    style={{ display: "inline-block" }}
-  >
-    <DropdownToggle style={{ background: "rgb(0, 0, 0)" }}>
-      {name}
-    </DropdownToggle>
-    <DropdownMenu>{item(routes)}</DropdownMenu>
-  </Dropdown>
-);
+class CommentBox extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    console.log(`props: ${props}${JSON.stringify(props)}`);
+    this.state = {
+      isOpen: false
+    };
+  }
 
-const item = (routes: any) =>
-  routes.map((route: any) => {
-    if (route.name === undefined) {
-      return null;
-    } else {
+  changeMenuIsOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  items = () => (
+    <Dropdown
+      isOpen={this.state.isOpen}
+      toggle={this.changeMenuIsOpen}
+      // direction="right"
+      style={{ display: "inline-block" }}
+    >
+      <DropdownToggle style={{ background: "rgb(0, 0, 0)" }}>
+        {this.props.name !== undefined
+          ? this.props.name
+          : this.props.routes[0].name}
+      </DropdownToggle>
+      <DropdownMenu>{this.item(this.props.routes)}</DropdownMenu>
+    </Dropdown>
+  );
+
+  item = (routes: any) =>
+    routes.map((route: any) => {
+      if (route.name === undefined) {
+        return null;
+      } else {
+        return (
+          <DropdownItem>
+            <li key={`${route.path}`}>
+              <Link to={route.path}>{route.name}</Link>
+            </li>
+          </DropdownItem>
+        );
+      }
+    });
+
+  render(): any {
+    const nextTarget = this.props.routes.find(
+      (route: any) =>
+        route.child !== undefined &&
+        window.location.pathname.match(route.path) !== null
+    );
+
+    if (nextTarget !== undefined) {
+      console.log(`nextTarget is  ${JSON.stringify(nextTarget)}`);
       return (
-        <DropdownItem>
-          <li key={`${route.path}`}>
-            <Link to={route.path}>{route.name}</Link>
-          </li>
-        </DropdownItem>
+        <div style={{ display: "inline-block" }}>
+          {this.items()}
+          <CommentBox routes={nextTarget.child} name={nextTarget.name} />
+        </div>
       );
+    } else {
+      return <div style={{ display: "inline-block" }}>{this.items()}</div>;
     }
-  });
+  }
+}
 
-const Header = ({
-  routes = _routes,
-  isOpen,
-  changeMainMenuIsOpen,
-  changeSubMenuIsOpen
-}: any): any => {
-  const target = routes.find(
-    (route: any) =>
-      route.child !== undefined &&
-      window.location.pathname.match(route.path) !== null
-  );
-
-  return (
-    <div>
-      {items(routes, isOpen.main, changeMainMenuIsOpen, "Main")}
-      {target === undefined
-        ? null
-        : items(target.child, isOpen.sub, changeSubMenuIsOpen, target.name)}
-    </div>
-  );
-};
+const Header = ({ routes = _routes }: any): any => (
+  <CommentBox routes={routes} />
+);
 
 export default Header;
